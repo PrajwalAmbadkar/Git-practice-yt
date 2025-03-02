eea436b680bd96250bd7932461dded6be11a4016
PrajwalAmbadkar <prajwalambadkar12345@gmail.com>
2025-03-02 18:15:12 +0530

    changes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-def addition(a,b,d,e):
-    return a+b+d+e
+def addition(a,b):
+    return a+b
 
-def substraction(a,b,e):
-    return a-b-e
+def substraction(a,b,d):
+    return a-b-d
 
 def division(a,b):
     return a/b
